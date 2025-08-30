e4c4d14f38453dfe05e3c2054e69250355bba28f
Caroline Ann <cayrolann23@gmail.com>
2025-08-30 13:08:53 +0800

    Added skeleton value for total properties in routes.py

--- app/routes.py
+++ app/routes.py
@@ -6,27 +6,10 @@ from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User 
  
 @app.route('/')
-<<<<<<< HEAD
 @app.route('/home') 
 def index(): 
+    properties = 127
     return render_template('index.html', title='Home', total_properties=properties)
-=======
-@app.route('/index') 
-@login_required 
-def index(): 
-    user = {'username': 'Miguel'}
-    posts = [ 
-        { 
-            'author': {'username': 'John'}, 
-            'body': 'Beautiful day in Portland!' 
-        }, 
-        { 
-            'author': {'username': 'Susan'}, 
-            'body': 'The Avengers movie was so cool!' 
-        } 
-    ] 
-    return render_template("index.html", title='Home Page', posts=posts)
->>>>>>> a70ddfc4ac94acdbe249e7f44b6f630222d8d850
 
 @app.route('/login', methods=['GET', 'POST']) 
 def login(): 
