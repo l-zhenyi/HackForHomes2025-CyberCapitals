d118e0cb7f4968571fa9249262e15cc276f309e1
Caroline Ann <cayrolann23@gmail.com>
2025-08-30 12:30:02 +0800

    Modified routes.py for home.html

--- app/routes.py
+++ app/routes.py
@@ -3,20 +3,9 @@ from app import app
 from app.forms import LoginForm 
  
 @app.route('/')
-@app.route('/index') 
+@app.route('/home') 
 def index(): 
-    user = {'username': 'Miguel'}
-    posts = [ 
-        { 
-            'author': {'username': 'John'}, 
-            'body': 'Beautiful day in Portland!' 
-        }, 
-        { 
-            'author': {'username': 'Susan'}, 
-            'body': 'The Avengers movie was so cool!' 
-        } 
-    ] 
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', total_properties=properties)
 
 @app.route('/login', methods=['GET', 'POST']) 
 def login(): 
